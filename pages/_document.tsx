cfc1275f026eb24ad32953e77ba5875eec00c699
Mostafa Waleeed <mostafawaleed206@gmail.com>
2022-08-01 17:24:05 +0200

    add font files

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,41 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-        <link rel="preload" href="/fonts/fonts.css" as="style" />
+        <link
+          rel="preload"
+          href="/fonts/Noto.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/RobotoMono-Regular.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/SuisseIntl-Bold.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/SuisseIntl-Medium.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/SuisseIntl.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
         <link rel="icon" type="image/svg" sizes="32x32" href="/mw-logo.svg" />
         <link rel="apple-touch-icon" sizes="180x180" href="/mw-logo.svg" />
         <meta content="#ffffff" name="theme-color" />
