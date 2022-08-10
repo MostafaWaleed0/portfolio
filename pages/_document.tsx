1e8d38c19f26f65e9f37f2c5b5604ddc5ffd422c
Mostafa Waleeed <mostafawaleed206@gmail.com>
2022-08-10 11:18:30 +0200

    put them  all fonts in the css file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,41 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link
-          rel="preload"
-          href="/fonts/Noto.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/RobotoMono-Regular.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/SuisseIntl-Bold.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/SuisseIntl-Medium.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/SuisseIntl.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
+        <link rel="preload" href="/fonts/fonts.css" as="style" />
         <link rel="icon" type="image/svg" sizes="32x32" href="/mw-logo.svg" />
         <link rel="apple-touch-icon" sizes="180x180" href="/mw-logo.svg" />
         <meta content="#ffffff" name="theme-color" />
