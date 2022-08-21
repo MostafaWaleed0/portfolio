518081fdc53f9b3b6f4aab43b47e405ac3bbcf1b
Mostafa Waleeed <mostafawaleed206@gmail.com>
2022-08-21 15:26:06 +0200

    remove unused packages

--- next.config.js
+++ next.config.js
@@ -67,8 +67,3 @@ const securityHeaders = [
     value: 'camera=(), microphone=(), geolocation=()',
   },
 ]
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-module.exports = withBundleAnalyzer({})
