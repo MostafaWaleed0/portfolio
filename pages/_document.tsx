ceeb22f08da1f1ab9bb730c54781008b88ee550f
Mostafa Waleeed <mostafawaleed206@gmail.com>
2022-08-21 14:41:09 +0200

    change the structure of the public folder

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,8 +6,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preload" href="/fonts/fonts.css" as="style" />
-        <link rel="icon" type="image/svg" sizes="32x32" href="/mw-logo.svg" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/mw-logo.svg" />
+        <link rel="icon" type="image/svg" sizes="32x32" href="/static/favicons/mw-logo.svg" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/mw-logo.svg" />
         <meta content="#ffffff" name="theme-color" />
         <meta content="#ffffff" name="msapplication-TileColor" />
       </Head>
