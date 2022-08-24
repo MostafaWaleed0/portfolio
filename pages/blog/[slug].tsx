dcb0723add3c815b220c358da76e28046a93b8a7
Mostafa Waleeed <mostafawaleed206@gmail.com>
2022-08-24 06:03:45 +0200

    optimize format MDX files

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,45 +1,46 @@
-import { readdirSync, readFileSync } from 'fs'
-import { join }  from 'path'
-import matter from 'gray-matter'
-import Container from '../../components/Container'
-import Time from '../../components/Time'
-import hljs from 'highlight.js';
-import { marked } from 'marked'
+import { readdirSync, readFileSync } from 'fs';
+import { join } from 'path';
+import matter from 'gray-matter';
+import Container from 'components/Container';
+import Time from 'components/Time';
+import { mdxToHtml } from 'lib/mdx';
+import { MDXRemote } from 'next-mdx-remote';
 
 interface Props {
   slug: any;
-  frontmatter:{
+  frontmatter: {
     card: boolean;
     title: string | number | boolean;
     tag: string | number | boolean;
     description: string | number | boolean;
     date: {
-      day: number,
-      month: number,
-      year: number
+      day: number;
+      month: number;
+      year: number;
     };
-  },
+  };
   content: any;
 }
 
-
 export default function PostPage({
-  frontmatter: { title, date: {day, month, year} },
-  content,
+  frontmatter: {
+    title,
+    date: { day, month, year }
+  },
+  content
 }: Props) {
-
   return (
     <Container>
       <article className="[ wrapper ] [ region ] [ margin-block-start-300 ]">
         <div className="[ post ] [ flow ]">
           <header>
             <h1>{title}</h1>
-              <div className="cluster" data-align="start">
-                <Time day={day} month={month} year={year} separator={undefined}/>
-              </div>
+            <div className="cluster" data-align="start">
+              <Time day={day} month={month} year={year} />
+            </div>
           </header>
           <hr />
-          <div className="flow" dangerouslySetInnerHTML={{__html: marked(content)}}/>
+          <MDXRemote {...content} />
         </div>
       </article>
     </Container>
@@ -49,32 +50,19 @@ export default function PostPage({
 export async function getStaticPaths() {
   const files = readdirSync(join('data/posts'));
   const paths = files.map((filename) => ({
-    params: {slug: filename.replace('.mdx', '')},
-  }))
-  return {paths, fallback: false};
+    params: { slug: filename.replace('.mdx', '') }
+  }));
+  return { paths, fallback: false };
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  const markdownWithMeta = readFileSync(join('data/posts', slug + '.mdx'), 'utf-8');
+  const markdownWithMeta = readFileSync(
+    join('data/posts', slug + '.mdx'),
+    'utf-8'
+  );
   const { data: frontmatter, content } = matter(markdownWithMeta);
+  const { html, readingTime } = await mdxToHtml(content);
   return {
-    props: {frontmatter, slug, content},
-  }
+    props: { frontmatter, slug, content: html, readingTime }
+  };
 }
-
-marked.setOptions({
-  renderer: new marked.Renderer(),
-  highlight: function(code, lang) {
-    const language = hljs.getLanguage(lang) ? lang : 'plaintext';
-    return hljs.highlight(code, { language }).value;
-  },
-
-  langPrefix: 'language-', // highlight.js css expects a top-level 'hljs' class.
-  pedantic: false,
-  gfm: true,
-  breaks: false,
-  sanitize: false,
-  smartLists: true,
-  smartypants: false,
-  xhtml: false
-});
