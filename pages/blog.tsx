7d9196eff0162fb29e082158e8c16756655128dc
Mostafa Waleeed <mostafawaleed206@gmail.com>
2022-08-25 09:10:42 +0200

    make styles to blog cards

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -17,21 +17,45 @@ export default function Blog({ posts }: Props) {
       <article className="[ wrapper ] [ margin-block-start-700 margin-block-end-800 ]">
         <h1>Blogs</h1>
         <div className="margin-block-start-700">
-          <ol role="list">
+          <ol className="auto-grid" role="list" data-layout="blog">
             {posts.map((post, index) => {
-              return post.frontmatter.card == undefined &&
-                <li className="[ card ] [ flow ]" key={index + 1}>
-                  {/* <Image src={post.frontmatter.image} width={200} height={250} alt={post.frontmatter.image_alt}/> */}
-                  <header className="card__head">
-                    <h3 className="fs-600">{post.frontmatter.title}</h3>
-                    <div className="pill">{post.frontmatter.tag === undefined ? "Programming" : post.frontmatter.tag}</div>
-                  </header>
-                  <Time day={post.frontmatter.date.day} month={post.frontmatter.date.month} year={post.frontmatter.date.year} separator={undefined}/>
-                  <p>{post.frontmatter.description}</p>
-                  <Link href={`/blog/${post.slug}`}>
-                    <a className="button">Read More</a>
-                  </Link>
-                </li>
+              return (
+                post.frontmatter.card == undefined && (
+                  <li
+                    className="[ card ] [ focusable ]"
+                    key={index + 1}
+                    tabIndex={0}
+                  >
+                    <article className="flow" style={{ maxWidth: '561px' }}>
+                      {/* <Image
+                        src="https://images.pexels.com/photos/48770/business-time-clock-clocks-48770.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        width={561}
+                        height={300}
+                        alt=""
+                        layout="responsive"
+                        className="card__image"
+                      /> */}
+                      <h3 className="fs-600">{post.frontmatter.title}</h3>
+                      <div className="card__data">
+                        <Time
+                          day={post.frontmatter.date.day}
+                          month={post.frontmatter.date.month}
+                          year={post.frontmatter.date.year}
+                        />
+                        <div className="pill">
+                          {post.frontmatter.tag === undefined
+                            ? 'Programming'
+                            : post.frontmatter.tag}
+                        </div>
+                      </div>
+                      <p className="fs-300">{post.frontmatter.description}</p>
+                      <Link href={`/blog/${post.slug}`}>
+                        <a className="button">Read More</a>
+                      </Link>
+                    </article>
+                  </li>
+                )
+              );
             })}
           </ol>
         </div>
