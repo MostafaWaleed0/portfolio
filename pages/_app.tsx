96934655193a475f0d06a8a356974260f338b053
Mostafa Waleeed <mostafawaleed206@gmail.com>
2022-12-03 16:00:22 +0200

    ✨ feat: adding `@vercel/analytics`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import '../assets/css/style.css';
 
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="data-theme">
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
