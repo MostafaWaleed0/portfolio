b44b1791c5007916ee090709960bf282e111de31
Mostafa Waleeed <mostafawaleed206@proton.me>
2023-02-22 12:09:27 +0200

    adding `twitter:creator`

--- components/Container.tsx
+++ components/Container.tsx
@@ -34,6 +34,7 @@ export default function Container(props) {
         <meta name="googlebot" content="follow, index" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@MostafaAmr206" />
+        <meta name="twitter:creator" content="@MostafaAmr206" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
