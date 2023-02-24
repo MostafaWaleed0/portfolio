950456832e3fbab3843292bfbf96bf0f073e6029
Mostafa Waleeed <mostafawaleed206@proton.me>
2023-02-24 11:48:12 +0200

    Fix invalid DOM property

--- components/icons/Github.tsx
+++ components/icons/Github.tsx
@@ -4,7 +4,7 @@ const Github = (props: SVGProps<SVGSVGElement>) => (
   <svg
     stroke="currentColor"
     fill="currentColor"
-    stroke-width="0"
+    strokeWidth="0"
     viewBox="0 0 24 24"
     height="1.4em"
     width="1.4em"
