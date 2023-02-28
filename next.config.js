b5c1aea23c3a1b0f03d641ba96733ac5bdb4f50b
Mostafa Waleeed <mostafawaleed206@proton.me>
2023-02-28 18:39:27 +0200

    set URL patterns in `next.config.js`

--- next.config.js
+++ next.config.js
@@ -5,11 +5,14 @@
 
 const nextConfig = {
   images: {
-    domains: [
-      'image/png',
-      'image/webp',
-      's3-alpha.figma.com',
-      'images.pexels.com'
+    formats: ['image/avif', 'image/webp'],
+
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/**'
+      }
     ]
   },
 
