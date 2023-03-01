012414d8e5d0b16f6672fedeb79dc333d378f3d8
Mostafa Waleeed <mostafawaleed206@proton.me>
2023-03-01 13:45:24 +0200

    Change the font size of the error message

--- app/error.tsx
+++ app/error.tsx
@@ -3,7 +3,7 @@
 export default function NotFound() {
   return (
     <div className="flex-row">
-      <h1 className="margin-auto fs-1000">
+      <h1 className="margin-auto fs-700">
         Oops! Something went wrong. Please check your code and try again.
       </h1>
     </div>
