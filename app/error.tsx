9a8ac494d4a95522843c31f6791dec2b181abed7
Mostafa Waleeed <mostafawaleed206@proton.me>
2023-03-01 13:44:38 +0200

    Change the error message

--- app/error.tsx
+++ app/error.tsx
@@ -3,7 +3,9 @@
 export default function NotFound() {
   return (
     <div className="flex-row">
-      <h1 className="margin-auto fs-1000">404</h1>
+      <h1 className="margin-auto fs-1000">
+        Oops! Something went wrong. Please check your code and try again.
+      </h1>
     </div>
   );
 }
