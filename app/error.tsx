7614ff52d8fa5a729ca416c078d0b431d0bed56b
Mostafa Waleeed <mostafawaleed206@proton.me>
2023-03-01 13:39:13 +0200

    Remove height from error page

--- app/error.tsx
+++ app/error.tsx
@@ -2,7 +2,7 @@
 
 export default function NotFound() {
   return (
-    <div className="flex-row" style={{ height: '100vh' }}>
+    <div className="flex-row">
       <h1 className="margin-auto fs-1000">404</h1>
     </div>
   );
