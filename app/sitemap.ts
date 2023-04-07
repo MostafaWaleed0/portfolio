81f6b081fd8dceb68877bdb8dc54bd984b736446
Mostafa Waleeed <mostafawaleed206@proton.me>
2023-04-07 14:00:16 +0200

    add `lastModified`  to the sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,11 +3,13 @@ import { sortPosts } from 'lib/sort';
 
 export default async function sitemap() {
   const posts = sortPosts(allPosts).map((post) => ({
-    url: `https://mostafawaleed.me/blog/${post.slug}`
+    url: `https://mostafawaleed.me/blog/${post.slug}`,
+    lastModified: post.date
   }));
 
   const routes = ['', 'blog', 'contact'].map((route) => ({
-    url: `https://mostafawaleed.me/${route}`
+    url: `https://mostafawaleed.me/${route}`,
+    lastModified: new Date().toISOString().split('T')[0]
   }));
 
   const allPages = [...routes, ...posts];
