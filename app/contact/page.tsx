d92403da3fc7ad2542ea76cdb101895fbcc198b3
Mostafa Waleeed <mostafawaleed206@proton.me>
2023-12-02 18:15:44 +0200

    Change contact title and description

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,18 +2,18 @@ import From from 'components/Form';
 import type { Metadata } from 'next/types';
 
 export const metadata: Metadata = {
-  title: 'Contact ',
+  title: 'Contact',
   description:
-    "If you want to know more about me or my work, I'll be happy to answer questions and share what I'm up to. You can contact me directly through the contact form on my website."
+    "If you want to know more about me or my work, I'll be happy to answer questions and share what I'm up to. You can contact me directly through the contact form."
 };
 
 export default function Contact() {
   return (
     <>
       <section className="[ wrapper ] [ margin-block-start-700 margin-block-end-800 ]">
-        <div className="[ headline ] [ flow ]">
-          <h1>Contact</h1>
-          <p>If you have any questions or want to work together? </p>
+        <div className="[ headline ] [ flow ]" data-headline-style="wide">
+          <h1>{String(metadata.title)}</h1>
+          <p>{metadata.description}</p>
         </div>
       </section>
       <section className="[ region ] [ bg-inverse ]">
